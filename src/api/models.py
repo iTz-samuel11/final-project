2b78d283b17c2e2fe7ab7b09bf7947a263fd9ed4
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-07-23 04:31:45 +0000

    base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    client = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __init__(self,email,password):
+        self.email=email
+        self.password=password
+        self.client= False
+
+        db.session.add(self)
+        db.session.commit()
+
 
     def __repr__(self):
         return f'<User {self.email}>'
