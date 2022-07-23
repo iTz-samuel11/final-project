aee5693d73548ca9256d2950bc94c58d4aa1d91b
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-07-23 04:40:21 +0000

    agrego vista

--- src/front/js/pages/SignUp.js
+++ src/front/js/pages/SignUp.js
@@ -6,34 +6,39 @@ export const SignUp = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div className="container">
-      <h1 className="m-3"> SignUp</h1>
-      <div className="row">
-        <form>
-          <h4 className="text-secondary">{" Email"}</h4>
-          <input
-            type="text"
-            name="email"
-            className="form-control m-1"
-            value={email}
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <h4 className="text-secondary"> {"Password"}</h4>
-          <input
-            type="password"
-            className="form-control m-1"
-            name="password"
-            value={password}
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </form>
+    <div className="container d-flex">
+      <div className="col-7">
+        <h1 className="m-3"> SignUp</h1>
+        <div className="row">
+          <form>
+            <h4 className="text-secondary">{" Email"}</h4>
+            <input
+              type="text"
+              name="email"
+              className="form-control m-1"
+              value={email}
+              placeholder="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <h4 className="text-secondary"> {"Password"}</h4>
+            <input
+              type="password"
+              className="form-control m-1"
+              name="password"
+              value={password}
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </form>
+        </div>
+        <div className="row">
+          <button type="button" className="btn btn-success m-4">
+            {"sign up..."}
+          </button>
+        </div>
       </div>
-      <div className="row">
-        <button type="button" className="btn btn-success m-4">
-          {"sign up..."}
-        </button>
+      <div className="col-5">
+        <h1>Hola</h1>
       </div>
     </div>
   );
