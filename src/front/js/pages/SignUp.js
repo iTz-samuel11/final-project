c637cb994ac89aa8233a2dee24471bae46e96681
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-07-23 04:20:48 +0000

    agrego planilla de sign up

---
+++ src/front/js/pages/SignUp.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const SignUp = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="container">
+      <h1 className="m-3"> SignUp</h1>
+      <div className="row">
+        <form>
+          <h4 className="text-secondary">{" Email"}</h4>
+          <input
+            type="text"
+            name="email"
+            className="form-control m-1"
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <h4 className="text-secondary"> {"Password"}</h4>
+          <input
+            type="password"
+            className="form-control m-1"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+      </div>
+      <div className="row">
+        <button type="button" className="btn btn-success m-4">
+          {"sign up..."}
+        </button>
+      </div>
+    </div>
+  );
+};
