2900a7950c81f7a472842bd32cb0488f5a71b8ae
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-07-25 19:15:56 +0000

    agrego hash al password

--- src/api/models.py
+++ src/api/models.py
@@ -1,28 +1,34 @@
 from flask_sqlalchemy import SQLAlchemy
+from base64 import b64encode
+from werkzeug.security import generate_password_hash
+import os
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    client = db.Column(db.Boolean(), unique=False, nullable=False)
     nombre= db.Column(db.String(120), nullable=False)
     apellido= db.Column(db.String(120), nullable= False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # password = db.Column(db.String(80), unique=False, nullable=False)
+    password_hash = db.Column(db.String(250), nullable=False, default="default")
+    user_salt = db.Column(db.String(120), nullable=False)
+    client = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __init__(self,email,password,nombre,apellido):
         self.email=email
-        self.password=password
+        # self.password=password
         self.client= False
         self.nombre=nombre
         self.apellido=apellido
+        self.password_hash = generate_password_hash(f"{password}{self.user_salt}")
+        self.user_salt = b64encode(os.urandom(32)).decode("utf-8")
 
         db.session.add(self)
         db.session.commit()
 
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(f"{password}{self.user_salt}", salt_length=len(self.user_salt))
 
     def serialize(self):
         return {
