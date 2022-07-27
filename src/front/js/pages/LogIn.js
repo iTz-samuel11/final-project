91c78e422103072f9138f0b59b10c10464db5949
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-07-27 02:33:07 +0000

    login

--- src/front/js/pages/LogIn.js
+++ src/front/js/pages/LogIn.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+// export const LogIn = (props) => {
+//   return <h1>login</h1>;
+// };
 
 export const LogIn = (props) => {
-  return <h1>login</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <div className="row">
+        <form>
+          <input
+            type="text"
+            name="email"
+            className="form-control m-1"
+            value={email}
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="form-control m-1"
+            name="password"
+            value={password}
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
+      </div>
+      <div className="row">
+        <button
+          type="button"
+          className="btn btn-primary m-4"
+          onClick={async (e) => {
+            const success = await actions.logIn({
+              email: email,
+              password: password,
+            });
+            if (success) {
+              navigate("/private");
+              return;
+            }
+            alert("something happened while creating the user");
+          }}
+        >
+          {" "}
+          {"Login"}{" "}
+        </button>
+      </div>
+    </div>
+  );
 };
