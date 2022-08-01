ac1c2f928e6ce1bb74a109ed134538ab57eedca2
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-08-01 22:28:51 +0000

    frontend en private sin probar

--- src/front/js/pages/Seguros.js
+++ src/front/js/pages/Seguros.js
@@ -10,13 +10,55 @@ import { NavPrincipal } from "../component/NavPrincipal";
 
 export const SegurosBonpland = () => {
   return (
-    <button
-      className="btn btn-danger me-4"
-      onClick={(e) => {
-        navigate("/SolicitudPoliza");
-      }}
-    >
-      {"Comprar poliza"}
-    </button>
+    <div>
+      <button
+        className="btn btn-danger me-4"
+        onClick={(e) => {
+          navigate("/SolicitudPoliza");
+        }}
+      >
+        {"Comprar poliza"}
+      </button>
+      <img
+        style={{
+          height: "400px",
+          width: "350x",
+          margin: "30%",
+          marginTop: "10%",
+          borderRadius: "10px",
+        }}
+        src="https://res.cloudinary.com/dn7fj8zhu/image/upload/v1659057236/Red_Emergency_Ambulance_Instagram_Post_efnfrl.png"
+      />
+      <img
+        style={{
+          height: "400px",
+          width: "350x",
+          margin: "30%",
+          marginTop: "10%",
+          borderRadius: "10px",
+        }}
+        src="https://res.cloudinary.com/dn7fj8zhu/image/upload/v1659056618/1_vuqord.jpg"
+      />
+      <img
+        style={{
+          height: "100px",
+          width: "100x",
+          margin: "50%",
+          marginTop: "15%",
+          borderRadius: "10px",
+        }}
+        src="https://res.cloudinary.com/dn7fj8zhu/image/upload/v1659056618/1_vuqord.jpg"
+      />
+      <img
+        style={{
+          height: "100px",
+          width: "50x",
+          margin: "25%",
+          marginTop: "5%",
+          borderRadius: "10px",
+        }}
+        src="https://res.cloudinary.com/dn7fj8zhu/image/upload/v1659392688/Red_Emergency_Ambulance_Instagram_Post_1_qh0u33.png"
+      />
+    </div>
   );
 };
