63f689c1207a49816bb51e63ec555b51ee0ff1aa
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-08-02 02:07:47 +0000

    adelanto enviar email
    instalo emailjs

--- src/front/js/pages/SolicitarPoliza.js
+++ src/front/js/pages/SolicitarPoliza.js
@@ -1,16 +1,39 @@
-import React, { useContext, useEffect } from "react";
-import { Nav } from "../component/Nav";
-import { Context } from "../store/appContext";
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 export const SolicitudPoliza = () => {
-  const { store, actions } = useContext(Context);
-  useEffect(() => {
-    actions.poliza();
-  }, [actions.poliza]);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_7a21d88",
+        "template_2d7b76r",
+        form.current,
+        "BVEQ65oAn7GRS9-Vk"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <React.Fragment>
-      <Nav />
-      <div>{`${store.poliza && store.poliza.poliza}`}</div>
-    </React.Fragment>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 };
