ea65d6150b0b60c776af2fef29699f89b4fe2421
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-08-07 05:08:18 +0000

    agrego otro mapa

--- src/front/js/component/MapView.js
+++ src/front/js/component/MapView.js
@@ -1,72 +1,100 @@
-import React, { useRef, useEffect, useState } from "react";
-import mapboxgl from "!mapbox-gl";
+// import React, { useState, useRef, useCallback, useState } from 'react'
+// import mapboxgl from "!mapbox-gl";
+// import 'mapbox-gl/dist/mapbox-gl.css'
+// import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoic2FtdWVsMTFmYXJhY28iLCJhIjoiY2w2aXB6d2x3MHFmazNkcWdqeTk1M2o0aSJ9.h375uchZCDZdNDEBXbL23g";
+// import MapGL from 'react-map-gl'
+// import Geocoder from 'map-gl-geocoder'
+
+// mapboxgl.accessToken =
+//   "pk.eyJ1Ijoic2FtdWVsMTFmYXJhY28iLCJhIjoiY2w2aXB6d2x3MHFmazNkcWdqeTk1M2o0aSJ9.h375uchZCDZdNDEBXbL23g";
+
+// export const MapView = () => {
+//   const mapContainer = useRef(null);
+//   const map = useRef(null);
+//   const [lng, setLng] = useState(-67.556104);
+//   const [lat, setLat] = useState(10.2197393);
+//   const [zoom, setZoom] = useState(9);
+
+//   useEffect(() => {
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: "mapbox://styles/mapbox/streets-v11",
+//       center: [lng, lat],
+//       zoom: zoom,
+//     });
+//   });
+//   useEffect(() => {
+//     if (!map.current) return; // wait for map to initialize
+//     map.current.on("move", () => {
+//       setLng(map.current.getCenter().lng.toFixed(4));
+//       setLat(map.current.getCenter().lat.toFixed(4));
+//       setZoom(map.current.getZoom().toFixed(2));
+//     });
+//   });
+
+// //   map.addControl(
+// //     new MapboxGeocoder({
+// //       accessToken: mapboxgl.accessToken,
+// //       mapboxgl: mapboxgl,
+// //     })
+// //   );
+//   return (
+//     <div>
+//       <div className="sidebar">
+//         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+//       </div>
+//       <div ref={mapContainer} className="map-container" />
+//     </div>
+//   );
+// };
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import React, { useState, useRef, useCallback } from "react";
+import MapGL from "react-map-gl";
+import Geocoder from "react-geocoder";
+
+// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
+const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoic2FtdWVsMTFmYXJhY28iLCJhIjoiY2w2aXE0Nmk5MGlmczNjcnh3N3Z3YWE5YyJ9.NcXOG8smUhjajrui9btWTg";
 
 export const MapView = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-67.556104);
-  const [lat, setLat] = useState(10.2197393);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+  const [viewport, setViewport] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
   });
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-
-    // map.on("load", function () {
-    //   // Add an image to use as a custom marker
-    //   map.loadImage(
-    //     "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
-    //     function (error, image) {
-    //       if (error) throw error;
-    //       map.addImage("custom-marker", image);
-    //       // Add a GeoJSON source with multiple points
-    //       map.addSource("points", {
-    //         type: "geojson",
-    //         data: {
-    //           type: "FeatureCollection",
-    //         },
-    //       });
-    //       // Add a symbol layer
-    //       map.addLayer({
-    //         id: "points",
-    //         type: "symbol",
-    //         source: "points",
-    //         layout: {
-    //           "icon-image": "custom-marker",
-    //           // get the title name from the source's "title" property
-    //           "text-field": ["get", "title"],
-    //           "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-    //           "text-offset": [0, 1.25],
-    //           "text-anchor": "top",
-    //         },
-    //       });
-    //     }
-    //   );
-    // });
-  });
+  const geocoderContainerRef = useRef();
+  const mapRef = useRef();
+  const handleViewportChange = useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
 
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+    <div style={{ height: "100vh" }}>
+      <div
+        ref={geocoderContainerRef}
+        style={{ position: "absolute", top: 20, left: 20, zIndex: 1 }}
+      />
+      <MapGL
+        ref={mapRef}
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={handleViewportChange}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      >
+        <Geocoder
+          mapRef={mapRef}
+          containerRef={geocoderContainerRef}
+          onViewportChange={handleViewportChange}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          position="top-left"
+        />
+      </MapGL>
     </div>
   );
 };
